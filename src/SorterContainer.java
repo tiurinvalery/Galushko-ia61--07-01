d4fef5702b651cb8cfd6173f32f2e4f87cb13875
Caernarvon <zvirusmail@gmail.com>
2018-02-11 23:50:21 +0200

    added new bubblesort

--- src/SorterContainer.java
+++ src/SorterContainer.java
@@ -1,35 +1,23 @@
 public class SorterContainer {
     public static void main(String args[]){
         int [] arr = {8,100,27,-3,4,56,32,1,67};
-        bubbleSort(arr);
-        for(int i =0;i<arr.length;i++){
-            System.out.println(arr[i]);
-        }
+        dmytroBubbleSort(arr);
     }
 
-    public static void bubbleSort(int[] array) {
-        for (int j = array.length; j > 0; j--) {
-            for (int i = 1; i < array.length; i++) {
-                int temp = 0;
-                if (array[i - 1] > array[i]) {
-                    temp = array[i - 1];
-                    array[i - 1] = array[i];
-                    array[i] = temp;
+    public static void dmytroBubbleSort(int[] array) {
+        doBubbleSort(array);
+    }
+
+    private static void doBubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = array.length - 1; j > i; j--) {
+                if (array[j] < array[j - 1]) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
                 }
             }
         }
     }
 
-    public static void NikaBubbleSort(int[] mas) {
-        for(int j = mas.length;j>0;j--){
-            for(int i =1;i < mas.length;i++ ) {
-                int temp =0;
-                if(mas[i-1]>mas[i]) {
-                    temp = mas[i-1];
-                    mas[i-1] = mas[i];
-                    mas[i]=temp;
-                }
-            }
-        }
-    }
 }
